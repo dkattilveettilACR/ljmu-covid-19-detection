24fe089b7fc59a0bec311aa720f2c7493cd1d262
deepakkvgit <deepak.kv@gmail.com>
2020-10-25 15:25:32 -0400

    Bugfix in generating images

--- gan_classifier/dcgan_ac_covid.py
+++ gan_classifier/dcgan_ac_covid.py
@@ -578,7 +578,7 @@ if __name__ == '__main__':
             classes = {0:"normal", 1:"bacterial", 2:"viral", 3:"covid"}
             batch_count = int(cnt/10)
             for num in range(batch_count):
-                noise1 = np.random.normal(0, 1, (10, 100))
+                noise = np.random.normal(0, 1, (10, 100))
                 sampled_labels = np.array([args.label for _ in range(4)])
                 gen_imgs = acgan.discriminator.predict([noise, sampled_labels])
                 # Rescale images 0 - 1
